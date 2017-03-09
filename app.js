03d74c706cd09a9eaa6148fe7299a1e974c81f3a
عبد الرحمن <deinondesign@gmail.com>
2017-03-09 16:57:08 +0000

    Add /api Router

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const app = express();
 // Port
 const port = process.env.PORT || 3000;
 
+const bookRouter = express.Router();
+
+bookRouter.route('/books')
+.get((req, res) => {
+    let responseJSON = {books:['Book 1','Book 2']};
+    res.json(responseJSON);
+});
+
+app.use('/api', bookRouter);
+
 app.get('/',(req, res)=> res.end('Index'));
 
 app.listen(port, () => console.log(`Listening on ${port}`));
